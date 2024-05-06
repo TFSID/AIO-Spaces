b9769268985e6d6036a89ca128bbaeb05d244780
ezazee <tegar.firman215@gmail.com>
2024-05-06 18:37:21 +0700

    update template structures

--- frontend/services/flask/main.py
+++ frontend/services/flask/main.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template
 from bs4 import BeautifulSoup
 
 # app = Flask(__name__, static_url_path='/static')
-app = Flask(__name__)
+
+app = Flask(__name__,template_folder='../../view',static_url_path='/frontend/public')
 
 @app.route('/')
 def hello():
@@ -14,15 +15,13 @@ def read_html_file(file_path):
 
 @app.route('/dashboard')
 def home():
-    # header = read_html_file('components/header.html')
-    # sidebar = read_html_file('frontend/components/sidebar.html')
-    # body = read_html_file('frontend/components/sidebar.html')
-    # footer = read_html_file('frontend/components/footer.html')
+    
+    
+    header = render_template('components/header.html')
     sidebar = render_template('components/sidebar.html')
-    # footer_html = read_html_file('organisms/footer.html')
-    template = render_template('components/header.html')
-    template += render_template('dashboard.html',sidebar=sidebar)
-    template += render_template('footer.html')
+    footer = render_template('components/footer.html')
+    # import dashboard pages
+    template = render_template('pages/dashboard.html',sidebar=sidebar, header=header)
     return template
 
 def main():
